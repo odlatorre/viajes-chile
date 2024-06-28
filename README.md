e0999a9de8c44d203dadb2441045395ecc8b05ea
Oscar <odlatorre@outlook.com>
2024-06-28 16:32:30 -0400

    Agregando Smooth Scroll

--- README.md
+++ README.md
@@ -61,3 +61,4 @@ tipo: Libreria Javascript
 ```HTML
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 ```
+
---
+++ assets/js/bootstrap-script.js
@@ -0,0 +1,4 @@
+
+// Agregando Tooltip de bootstrap
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
